e69b4004ad6c3db855635080cd260f349f25ed47
Ekaterina Bulycheva <ekaterinabul21@mail.ru>
2023-03-16 18:53:44 +0300

    fix: add icon-link to profile

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,16 +1,30 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import styles from './Header.module.css';
 import { ButtonWhite } from '../../atoms/ButtonWhite/ButtonWhite';
 import { Logo } from '../../atoms/Logo/Logo';
+import { getAccessTokenSelector } from '../../../redux/slices/userSlice';
 
 export function Header() {
+  const accessToken = useSelector(getAccessTokenSelector);
+
   return (
     <header className={styles.header}>
-      <Link to="/">
-        <Logo />
-      </Link>
+      <div className={styles.containerLogo}>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
       <div className={styles.containerAdmission}>
-        <ButtonWhite type="button"><Link to="/signin">Войти</Link></ButtonWhite>
+        {accessToken ? (
+          <Link to="/profile">
+            <ButtonWhite type="button"><i className="fa-regular fa-user" /></ButtonWhite>
+          </Link>
+        ) : (
+          <Link to="/signin">
+            <ButtonWhite type="button">Войти</ButtonWhite>
+          </Link>
+        )}
       </div>
     </header>
   );
