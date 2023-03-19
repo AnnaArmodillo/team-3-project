fe8044c1ff3040dfc1812551313de23e6b7342b9
Ekaterina Bulycheva <ekaterinabul21@mail.ru>
2023-03-20 00:01:16 +0300

    feat: implement deleting a survey from the list of visited surveys

--- src/components/molecules/SurveyItem/SurveyItem.jsx
+++ src/components/molecules/SurveyItem/SurveyItem.jsx
@@ -1,25 +1,51 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getUserSelector } from '../../../redux/slices/userSlice';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { getAccessTokenSelector, getUserSelector } from '../../../redux/slices/userSlice';
 import { getSurveyURL } from '../../../utils/helper';
 import styles from './surveyItem.module.css';
-import avatar from '../../../images/avatar.png';
+// import avatar from '../../../images/avatar.png';
+import { MC, SC, UC } from '../../../utils/constants';
+import { teamProjectApi } from '../../../api/TeamProjectApi';
 
 export function SurveyItem({ survey }) {
   const user = useSelector(getUserSelector);
+  const accessToken = useSelector(getAccessTokenSelector);
 
-  const isUserDone = survey.done.find((item) => item === user.id);
+  const isUserDone = survey.done.find((userId) => userId === user.id);
+  const isUserAuthor = survey.author === user.id;
+
+  const client = useQueryClient();
+  const {
+    mutateAsync,
+    // isLoading,
+    // isError,
+    // error,
+  } = useMutation({
+    mutationFn: () => teamProjectApi.deleteSurveyFromVisited(survey.surveyId, accessToken),
+    onSuccess: () => { client.invalidateQueries({ queryKey: ['VisitedSurveysFetch'] }); },
+  });
+
+  const deleteFromVisited = (e) => {
+    e.preventDefault();
+    mutateAsync();
+  };
+
+  // const deleteFromMySurveys = (e) => {
+  //   e.preventDefault();
+  //   console.log('опрос удален из базы');
+  // };
 
   const formatSurveysType = () => {
     let type = '';
     switch (survey.surveyType) {
-      case 'SC':
+      case SC:
         type = 'единственный тип выбора';
         break;
-      case 'MC':
+      case MC:
         type = 'множественный тип выбора';
         break;
-      case 'UC':
+      case UC:
         type = 'уникальный тип выбора';
         break;
 
@@ -53,16 +79,24 @@ export function SurveyItem({ survey }) {
                 </>
               )}
             </span>
-            <span>
+            {!isUserAuthor && (
+            <span onClick={deleteFromVisited}>
               <i className="fa-regular fa-trash-can" />
               <span className={styles.spanDelete}>удалить</span>
             </span>
+            )}
+            {/* {isUserAuthor && (
+            <span onClick={deleteFromMySurveys}>
+              <i className="fa-regular fa-trash-can" />
+              <span className={styles.spanDelete}>удалить</span>
+            </span>
+            )} */}
           </div>
         </div>
 
         <div className={styles.containerBottom}>
-          <img src={avatar} alt="avatar" />
-          <p>{user.name}</p>
+          {/* <img src={avatar} alt="avatar" />
+          <p>{user.name}</p> */}
         </div>
       </div>
     </Link>
