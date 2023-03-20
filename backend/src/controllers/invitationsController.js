7660e4c501c617dbd99783aaad53b36fee6955c5
Anna Denisova <abachledacurus@gmail.com>
2023-03-20 23:34:17 +0700

    На бэке добавлена отправка результатов, какие приглашения удалось отправить, какие нет

--- backend/src/controllers/invitationsController.js
+++ backend/src/controllers/invitationsController.js
@@ -6,25 +6,36 @@ function sendInvitations(req, res) {
     const { users, surveyId } = req.body;
     const token = req.headers.authorization.split(' ')[1];
     const userID = getUserIdFromToken(token);
+    const usersSuccess = [];
+    const usersFail = [];
+    console.log(users);
     try {
       users.map((userFromReq) => {
-        const currentUser = DB.users.find((user) => user.email === userFromReq.email);
-        currentUser.invitations.push({
-          survey: surveyId,
-          fromUser: userID,
-        })
+        const currentUser = DB.users.find(
+          (user) => user.email === userFromReq.email
+        );
+        if (currentUser) {
+          currentUser.invitations.push({
+            survey: surveyId,
+            fromUser: userID,
+          });
+          usersSuccess.push(userFromReq.email);
+        } else {
+          usersFail.push(userFromReq.email);
+        }
       });
       const newContent = `export const DB = ${JSON.stringify(DB)}`;
       updateDB(newContent);
     } catch (error) {
       return res.status(404).json('Пользователь с таким email не найден');
     }
-    return res.sendStatus(201);
+    const response = { usersSuccess, usersFail };
+    return res.status(201).json(response);
   } catch (error) {
     return res.sendStatus(500);
   }
 }
 
 export const invitationsController = {
-    sendInvitations,
+  sendInvitations,
 };
