802d68ab71c488c9db375620a6d9dafe1f1a7189
Yvette Demidova <yvette_demidova@mail.ru>
2023-03-22 06:04:49 +0300

    feat: В компонент страницы "Все опросы" добавлена возможность поиска по каталогу всех опросов в базе данных на сервере

--- src/components/pages/SurveysCatalogPage/SurveysCatalogPage.jsx
+++ src/components/pages/SurveysCatalogPage/SurveysCatalogPage.jsx
@@ -1,47 +1,37 @@
 import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+// import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { teamProjectApi } from '../../../api/TeamProjectApi';
+import { getSearchSelector } from '../../../redux/slices/filterSlice';
+import { getAccessTokenSelector } from '../../../redux/slices/userSlice';
 import { SURVEYS_CATALOG_PAGE } from '../../../utils/constants';
-import { Loader } from '../../Loader/Loader';
+import { withQuery } from '../../HOCs/withQuery';
+// import { Loader } from '../../Loader/Loader';
 import { SurveyItem } from '../../molecules/SurveyItem/SurveyItem';
+import Search from '../../organisms/Search/Search';
 import { MainWrap } from '../../templates/MainWrap/MainWrap';
 import styles from './surveysCatalogPage.module.css';
 
-export function SurveysCatalogPage() {
-  const {
-    data,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: [SURVEYS_CATALOG_PAGE],
-    queryFn: () => teamProjectApi.getAllSurveys(),
-  });
-  if (isLoading) {
-    return (
-      <MainWrap>
-        <Loader />
-      </MainWrap>
-    );
-  }
-  if (isError) {
-    return (
-      <MainWrap>
-        <h1>
-          Произошла ошибка
-          {' '}
-          {error.message}
-        </h1>
-      </MainWrap>
-    );
-  }
-  // console.log({ data });
-  if (data) {
+function SurveysCatalogPageInner({ surveys, search }) {
+  console.log({ surveys });
+  if (surveys) {
     return (
       <MainWrap>
         <div className={styles.surveysCatalogPage}>
-          <h1>Все опросы</h1>
+          <Search />
+          <h1>
+            {search
+              ? (
+                ('Опросы по запросу')
+              )
+              : (
+                ('Все опросы')
+              )}
+          </h1>
           <div className={styles.surveysCatalogContainer}>
-            {data.map((survey) => (
+            {surveys.map((survey) => (
               <div key={survey.surveyId} className={styles.surveyWr}>
                 <SurveyItem survey={survey} />
               </div>
@@ -52,3 +42,68 @@ export function SurveysCatalogPage() {
     );
   }
 }
+const SurveysCatalogPageInnerWithQuery = withQuery(SurveysCatalogPageInner);
+function SurveysCatalogPage() {
+  const accessToken = useSelector(getAccessTokenSelector);
+  const navigate = useNavigate();
+  const search = useSelector(getSearchSelector);
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/signin');
+    }
+  }, [accessToken]);
+
+  // console.log('search before useQuery', search);
+  // console.log('!search', !search);
+  // console.log('!!search', !!search);
+  const {
+    data: surveys = [],
+    isError,
+    error,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: [SURVEYS_CATALOG_PAGE, search],
+    queryFn: () => teamProjectApi.getAllSurveys(),
+    enabled: !search,
+  });
+
+  const {
+    data: surveysFromSearch = [],
+    isError: isErrorFromSearch,
+    error: errorFromSearch,
+    isLoading: isLoadingFromSearch,
+    refetch: refetchFromSearch,
+  } = useQuery({
+    queryKey: [SURVEYS_CATALOG_PAGE, search],
+    queryFn: () => teamProjectApi.searchSurveys(search),
+    enabled: !!search,
+  });
+
+  if (search) {
+    return (
+      <SurveysCatalogPageInnerWithQuery
+        surveys={surveysFromSearch}
+        isError={isErrorFromSearch}
+        isLoading={isLoadingFromSearch}
+        error={errorFromSearch}
+        refetch={refetchFromSearch}
+        search={search}
+      />
+    );
+  }
+
+  return (
+    <SurveysCatalogPageInnerWithQuery
+      surveys={surveys}
+      isError={isError}
+      isLoading={isLoading}
+      error={error}
+      refetch={refetch}
+      search={search}
+    />
+  );
+}
+
+export default SurveysCatalogPage;
