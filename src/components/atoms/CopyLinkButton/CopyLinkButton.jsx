1b2a13e2399c754591ee308fdfe84ac531b8d168
Anna Denisova <abachledacurus@gmail.com>
2023-03-23 17:15:24 +0700

    Стилизовано всплывающее уведомление

--- src/components/atoms/CopyLinkButton/CopyLinkButton.jsx
+++ src/components/atoms/CopyLinkButton/CopyLinkButton.jsx
@@ -1,34 +1,30 @@
-import { Slide, toast, ToastContainer } from 'react-toastify';
+import { Slide, toast } from 'react-toastify';
 import { getSurveyURL } from '../../../utils/helper';
 import styles from './copyLinkButton.module.css';
 
 export function CopyLinkButton({ surveyId }) {
-  const customId = 'custom-id-yes';
+  const customId = 'toastId';
   return (
-    <>
-      <button
-        type="button"
-        title="копировать ссылку"
-        className={styles.buttonCopy}
-        onClick={() => {
-          navigator.clipboard.writeText(getSurveyURL(surveyId));
-          toast.success('Ссылка скопирована', {
-            autoClose: 2000,
-            transition: Slide,
-            bodyClassName: `${styles.toastBody}`,
-            toastId: customId,
-            theme: 'colored',
-            closeButton: false,
-            rtl: true,
-          });
-        }}
-      >
-        <i className="fa-solid fa-copy" />
-      </button>
-      <ToastContainer
-        className={styles.toast}
-        autoClose
-      />
-    </>
+    <button
+      type="button"
+      title="копировать ссылку"
+      className={styles.buttonCopy}
+      onClick={() => {
+        navigator.clipboard.writeText(getSurveyURL(surveyId));
+        toast.success('Ссылка скопирована', {
+          autoClose: 2000,
+          transition: Slide,
+          className: `${styles.toast}`,
+          bodyClassName: `${styles.toastBody}`,
+          toastId: customId,
+          hideProgressBar: true,
+          theme: 'colored',
+          closeButton: false,
+          rtl: false,
+        });
+      }}
+    >
+      <i className="fa-solid fa-copy" />
+    </button>
   );
 }
