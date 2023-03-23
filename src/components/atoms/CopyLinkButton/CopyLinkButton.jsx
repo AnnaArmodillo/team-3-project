61fdbceb8f910acc22e1602721fd6e911ccb3553
Anna Denisova <abachledacurus@gmail.com>
2023-03-23 15:53:40 +0700

    Добавлено всплывающее уведомление при копировании ссылки

--- src/components/atoms/CopyLinkButton/CopyLinkButton.jsx
+++ src/components/atoms/CopyLinkButton/CopyLinkButton.jsx
@@ -1,15 +1,34 @@
+import { Slide, toast, ToastContainer } from 'react-toastify';
 import { getSurveyURL } from '../../../utils/helper';
 import styles from './copyLinkButton.module.css';
 
 export function CopyLinkButton({ surveyId }) {
+  const customId = 'custom-id-yes';
   return (
-    <button
-      type="button"
-      title="копировать ссылку"
-      className={styles.buttonCopy}
-      onClick={() => navigator.clipboard.writeText(getSurveyURL(surveyId))}
-    >
-      <i className="fa-solid fa-copy" />
-    </button>
+    <>
+      <button
+        type="button"
+        title="копировать ссылку"
+        className={styles.buttonCopy}
+        onClick={() => {
+          navigator.clipboard.writeText(getSurveyURL(surveyId));
+          toast.success('Ссылка скопирована', {
+            autoClose: 2000,
+            transition: Slide,
+            bodyClassName: `${styles.toastBody}`,
+            toastId: customId,
+            theme: 'colored',
+            closeButton: false,
+            rtl: true,
+          });
+        }}
+      >
+        <i className="fa-solid fa-copy" />
+      </button>
+      <ToastContainer
+        className={styles.toast}
+        autoClose
+      />
+    </>
   );
 }
