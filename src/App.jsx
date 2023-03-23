71c2611f1a5bab4baa8728ca4a315f8bfec13c08
lev33 <48180783+lev33@users.noreply.github.com>
2023-03-23 11:39:23 +0300

    add react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import { Footer } from './components/organisms/Footer/Footer';
 import { Header } from './components/organisms/Header/Header';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <ToastContainer />
     </div>
   );
 }
