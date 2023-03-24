604301c299d657e77ea1c8727862d3606f0b6933
Ekaterina Bulycheva <ekaterinabul21@mail.ru>
2023-03-24 08:35:34 +0300

    feat: добавляет анимацию на страницу Справка и на кнопки

--- src/components/atoms/ButtonWhite/ButtonWhite.jsx
+++ src/components/atoms/ButtonWhite/ButtonWhite.jsx
@@ -1,10 +1,16 @@
+import { motion } from 'framer-motion';
 import styles from './buttonWhite.module.css';
 
 export function ButtonWhite({ children, type, onClick }) {
   return (
     // eslint-disable-next-line react/button-has-type
-    <button type={type} className={styles.button} onClick={onClick}>
+    <motion.button
+      type={type}
+      className={styles.button}
+      onClick={onClick}
+      whileTap={{ scale: 0.95 }}
+    >
       {children}
-    </button>
+    </motion.button>
   );
 }
--- src/components/molecules/AccordionItem/AccordionItem.jsx
+++ src/components/molecules/AccordionItem/AccordionItem.jsx
@@ -1,9 +1,31 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import styles from './accordionItem.module.css';
 
 export function AccordionItem({ accordionItem }) {
   const [isActive, setIsActive] = useState(false);
+
+  const accordionContent = () => (
+    <motion.div
+      className={styles.accordionContent}
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      exit={{ opacity: 0, y: -50 }}
+    >
+      <div
+        className={styles.containerText}
+      >
+        {accordionItem.content.description}
+      </div>
+      <div
+        className={styles.containerVideo}
+      >
+        <video src={accordionItem.content.video} controls />
+      </div>
+    </motion.div>
+  );
 
   return (
     <div className={styles.accordionItem}>
@@ -22,15 +44,9 @@ export function AccordionItem({ accordionItem }) {
           )}
         </div>
       </div>
-      {isActive && (
-        <div className={styles.accordionContent}>
-          <div className={styles.containerText}>{accordionItem.content.description}</div>
-          <div className={styles.containerVideo}>
-            <video src={accordionItem.content.video} controls />
-          </div>
-        </div>
-      )}
-
+      <AnimatePresence>
+        {isActive && accordionContent()}
+      </AnimatePresence>
     </div>
   );
 }
